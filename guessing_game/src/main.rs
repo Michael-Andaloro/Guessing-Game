c0271b6ed677fe39bcd208d64c59f874318895f1
Michael Andaloro <michaelandaloro@Michaels-MacBook-Air.local>
2020-01-28 08:56:51 -0600

    Created first bit of the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,21 @@
+// Allows us to accept user input
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+	println!("Please input your guess.");
+
+	// let foo = bar;
+	// let foo = 5; // immutable
+	// let mut bar = 5; // Mutable/Changeable
+
+	// Creates a mutable (changeable) variable
+	let mut guess = String::new();
+
+	// Error handling
+	io::stdin().read_line(&mut guess)
+		.expect("Failed to read line");
+
+    // Print the user's input
+    println!("You guessed: {}", guess);
 }
